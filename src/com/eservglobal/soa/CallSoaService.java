ac7774ea07b5aa2d40a823cc0e3f2fd4d939e6bc
Darius <popescu88darius@gmail.com>
2017-08-14 14:08:14 +0300

    5th commit

--- src/com/eservglobal/soa/CallSoaService.java
+++ src/com/eservglobal/soa/CallSoaService.java
@@ -142,8 +142,8 @@ public class CallSoaService {
         else
             return ("unknown");
     }
-    public static void main(String[] args) {
+ /*   public static void main(String[] args) {
         CallSoaService callService= new CallSoaService();
-    }
+    }*/
 
 }

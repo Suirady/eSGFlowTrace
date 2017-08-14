e6cb1ba0325853a0a82f29e6405eb4d69d66ceba
Darius <popescu88darius@gmail.com>
2017-08-14 20:05:37 +0300

    6th commit

--- src/com/eservglobal/soa/ComponentData.java
+++ src/com/eservglobal/soa/ComponentData.java
@@ -6,13 +6,17 @@ import oracle.soa.management.facade.CompositeInstance;
 import oracle.soa.management.facade.Locator;
 import oracle.soa.management.util.ComponentInstanceFilter;
 import oracle.soa.management.util.CompositeInstanceFilter;
-
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -24,7 +28,7 @@ import java.util.logging.Logger;
 
 public class ComponentData {
 
-    private static final String filename = "AuditTrail.txt";
+    private static String filename;
     private static ComponentData componentData;
 
     private String instanceID;
@@ -63,9 +67,10 @@ public class ComponentData {
     }
 
     public void storeAuditTrail() throws Exception {
-        Path path = Paths.get(filename);
+        File temp = File.createTempFile("auditTrail", ".tmp");
+        filename =  temp.getAbsolutePath();
         // try-with-resources
-        try (BufferedWriter bw = Files.newBufferedWriter(path)) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(temp))) {
             if (getLoc() != null) {
                 CompositeInstanceFilter compositeInFilter = new CompositeInstanceFilter();
                 compositeInFilter.setId(instanceID);
@@ -77,85 +82,81 @@ public class ComponentData {
                 bw.newLine();
                 bw.newLine();
                 Iterator compositeInstancesIterator = compositeInstances.iterator();
-                if (!compositeInstancesIterator.hasNext()) {
-                    throw new NoSuchElementException();
-                } else {
-                    while (compositeInstancesIterator.hasNext()) {
-                        CompositeInstance compositeInstance = (CompositeInstance) compositeInstancesIterator.next();
-                        if (!exclusions.contains(compositeInstance.getCompositeDN().getCompositeName())) {
-                            bw.write(String.format("%s\n%s\n%s\n%s\n%s\n%s",
-                                    "Parent composite instance id:" + compositeInstance.getParentId(),
-                                    "Searched composite name: " + compositeInstance.getCompositeDN().getCompositeName(),
-                                    "Searched composite instance id:" + compositeInstance.getId(),
-                                    "Title :" + compositeInstance.getTitle(),
-                                    "Created date: " + compositeInstance.getCreationDate(),
-                                    "Domain name:" + compositeInstance.getCompositeDN().getDomainName()));
-                            bw.newLine();
+                while (compositeInstancesIterator.hasNext()) {
+                    CompositeInstance compositeInstance = (CompositeInstance) compositeInstancesIterator.next();
+                    if (!exclusions.contains(compositeInstance.getCompositeDN().getCompositeName())) {
+                        bw.write(String.format("%s\n%s\n%s\n%s\n%s\n%s",
+                                "Parent composite instance id: " + compositeInstance.getParentId(),
+                                "Searched composite name: " + compositeInstance.getCompositeDN().getCompositeName(),
+                                "Searched composite instance id: " + compositeInstance.getId(),
+                                "Title :" + compositeInstance.getTitle(),
+                                "Created date: " + compositeInstance.getCreationDate(),
+                                "Domain name: " + compositeInstance.getCompositeDN().getDomainName()));
+                        bw.newLine();
 
-                            switch (compositeInstance.getState()) {
-                                case CompositeInstance.STATE_COMPLETED_SUCCESSFULLY:
-                                    bw.write("State : COMPLETED_SUCCESSFULLY");
-                                    bw.newLine();
-                                    break;
-                                case CompositeInstance.STATE_FAULTED:
-                                    bw.write("State : STATE_FAULTED");
-                                    bw.newLine();
-                                    break;
-                                case CompositeInstance.STATE_RUNNING:
-                                    bw.write("State : STATE_RUNNING");
-                                    bw.newLine();
-                                    break;
-                                case CompositeInstance.STATE_STALE:
-                                    bw.write("State : STATE_STALE");
-                                    bw.newLine();
-                                    break;
-                                case CompositeInstance.STATE_RECOVERY_REQUIRED:
-                                    bw.write("State : STATE_RECOVERY_REQUIRED");
-                                    bw.newLine();
-                                    break;
-                                case CompositeInstance.STATE_SUSPENDED:
-                                    bw.write("State : STATE_SUSPENDED");
-                                    bw.newLine();
-                                    break;
-                                case CompositeInstance.STATE_TERMINATED_BY_USER:
-                                    bw.write("State : TERMINATED_BY_USER");
-                                    bw.newLine();
-                                    break;
-                                case CompositeInstance.STATE_UNKNOWN:
-                                    bw.write("State : STATE_UNKNOWN");
-                                    bw.newLine();
-                                    break;
-                                default:
-                                    bw.write("State : UNDEFINED");
-                                    bw.newLine();
-                            }
+                        switch (compositeInstance.getState()) {
+                            case CompositeInstance.STATE_COMPLETED_SUCCESSFULLY:
+                                bw.write("State : COMPLETED_SUCCESSFULLY");
+                                bw.newLine();
+                                break;
+                            case CompositeInstance.STATE_FAULTED:
+                                bw.write("State : STATE_FAULTED");
+                                bw.newLine();
+                                break;
+                            case CompositeInstance.STATE_RUNNING:
+                                bw.write("State : STATE_RUNNING");
+                                bw.newLine();
+                                break;
+                            case CompositeInstance.STATE_STALE:
+                                bw.write("State : STATE_STALE");
+                                bw.newLine();
+                                break;
+                            case CompositeInstance.STATE_RECOVERY_REQUIRED:
+                                bw.write("State : STATE_RECOVERY_REQUIRED");
+                                bw.newLine();
+                                break;
+                            case CompositeInstance.STATE_SUSPENDED:
+                                bw.write("State : STATE_SUSPENDED");
+                                bw.newLine();
+                                break;
+                            case CompositeInstance.STATE_TERMINATED_BY_USER:
+                                bw.write("State : TERMINATED_BY_USER");
+                                bw.newLine();
+                                break;
+                            case CompositeInstance.STATE_UNKNOWN:
+                                bw.write("State : STATE_UNKNOWN");
+                                bw.newLine();
+                                break;
+                            default:
+                                bw.write("State : UNDEFINED");
+                                bw.newLine();
+                        }
+                        bw.write("##################################################");
+                        bw.newLine();
+                        ComponentInstanceFilter filter1 = new ComponentInstanceFilter();
+                        filter1.setECID(compositeInstance.getECID());
+                        filter1.setOrderBy(ComponentInstanceFilter.ORDER_BY_CREATION_DATE_ASC);
+                        List<ComponentInstance> childComponentInstances = compositeInstance.getChildComponentInstances(filter1);
+                        Iterator<ComponentInstance> iterator = childComponentInstances.iterator();
+                        while (iterator.hasNext()) {
+                            ComponentInstance componentInstance = iterator.next();
+
+                            bw.write(String.format("%s\n%s\n%s\n%s\n%s",
+                                    "Child Component name :" + componentInstance.getComponentName(),
+                                    "Child Component instance id :" + componentInstance.getId(),
+                                    "State :" + componentInstance.getNormalizedStateAsString(),
+                                    "##################################################",
+                                    "Payload" + componentInstance.getAuditTrail()));
+                            bw.newLine();
                             bw.write("##################################################");
                             bw.newLine();
-                            ComponentInstanceFilter filter1 = new ComponentInstanceFilter();
-                            filter1.setECID(compositeInstance.getECID());
-                            filter1.setOrderBy(ComponentInstanceFilter.ORDER_BY_CREATION_DATE_ASC);
-                            List<ComponentInstance> childComponentInstances = compositeInstance.getChildComponentInstances(filter1);
-                            Iterator<ComponentInstance> iterator = childComponentInstances.iterator();
-                            while (iterator.hasNext()) {
-                                ComponentInstance componentInstance = iterator.next();
-
-                                bw.write(String.format("%s\n%s\n%s\n%s\n%s",
-                                        "Child Component name :" + componentInstance.getComponentName(),
-                                        "Child Component instance id :" + componentInstance.getId(),
-                                        "State :" + componentInstance.getNormalizedStateAsString(),
-                                        "##################################################",
-                                        "Payload" + componentInstance.getAuditTrail()));
-                                bw.newLine();
-                                bw.write("##################################################");
-                                bw.newLine();
-                            }
-                            bw.write("-----------------------------------------------------------");
                         }
-
+                        bw.write("-----------------------------------------------------------");
                     }
-                }
 
+                }
             }
+
         }
     }
 
@@ -166,10 +167,6 @@ public class ComponentData {
             CenterPanelController.areaTextP.setText(scan.next());
             CenterPanelController.saveFileBtnP.setVisible(true);
             scan.close();
-        } catch (NoSuchElementException ex) {
-            CenterPanelController.areaTextP.clear();
-            CenterPanelController.areaTextP.setText("Composite instance not found!");
-            CenterPanelController.saveFileBtnP.setVisible(false);
         } catch (IOException e) {
             Logger.getLogger(ComponentData.class.getName()).log(Level.SEVERE, null, e);
         }
